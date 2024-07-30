2101a6529ea62b417299148ff5268028abc70b15
hw1186 <ro49165703@gmail.com>
2024-07-30 05:42:41 +0000

    fix: renew

--- collector/elsasticsearch_collector_renew.py
+++ collector/elsasticsearch_collector_renew.py
@@ -1,37 +1,184 @@
-from elasticsearch_collector import elasticsearch
-from elasticsearch import AsyncElasticsearch
+from elasticsearch import AsyncElasticsearch, NotFoundError
+from fastapi import HTTPException
+import datetime
+import logging
+import socket
+from .fortigate_deployment import fortigate_parse
 
-class ElasticsearchCollector():
-    def __init__(self, system: str):
+class ElasticsearchCollector:
+    def __init__(self, system: str, TAG_NAME: str):
         self.system = system
-        self.es = AsyncElasticsearch([es_host]) # 생성 해야함 
+        self.TAG_NAME = TAG_NAME
+        self.es = AsyncElasticsearch("http://3.35.81.217:9200/")
+
+    def get_hostname(self, client_ip):
+        try:
+            return socket.gethostbyaddr(client_ip)[0]
+        except Exception as e:
+            logging.error(f"Failed to get hostname: {e}")
+            return client_ip
+
+    async def create_index_with_mapping(self, index_name, mapping):
+        try:
+            if not await self.es.indices.exists(index=index_name):
+                await self.es.indices.create(index=index_name, body={"mappings": mapping})
+        except Exception as e:
+            logging.error(f"Failed to create index with mapping: {e}")
+            raise HTTPException(status_code=500, detail=f"Failed to create index with mapping: {e}")
+
+    async def create_index_if_not_exists(self, index_name):
+        try:
+            await self.es.indices.get(index=index_name)
+        except NotFoundError:
+            await self.es.indices.create(index=index_name)
+
+    async def collect_logs(self, request, client_ip):
+        index_name = f"{self.system}_syslog"
+        for log in request:
+            if self.system == "fortigate":
+                log = await fortigate_parse(log['message'])
+            log['request_ip'] = client_ip
+            log['client_hostname'] = self.get_hostname(client_ip=client_ip)
+            log['TAG_NAME'] = self.TAG_NAME
+            await self.es.index(index=index_name, document=log)
+
+    async def save_integration(self, system, TAG_NAME, config):
+        index_name = "integration_info"
+        await self.create_index_if_not_exists(index_name)
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"SYSTEM": system}},
+                        {"match": {"TAG_NAME": TAG_NAME}}
+                    ]
+                }
+            }
+        }
+        res = await self.es.search(index=index_name, body=query)
+        if res['hits']['total']['value'] > 0:
+            raise HTTPException(status_code=400, detail=f"{TAG_NAME}이 이미 사용중입니다.")
+        log = {
+            "SYSTEM": system,
+            "TAG_NAME": TAG_NAME,
+            "inserted_at": datetime.datetime.now(),
+            "status": "started",
+            "config": config
+        }
+        response = await self.es.index(index=index_name, document=log)
+        print(f"{index_name}_log: {response['result']}")
+        return response
 
     async def get_status(self):
-        response = await self.es.get(index=f"{self.system}_logs", id=1)
-        return response["_source"]
-    
+        try:
+            query = {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {"match": {"SYSTEM": self.system}},
+                            {"match": {"TAG_NAME": self.TAG_NAME}}
+                        ]
+                    }
+                }
+            }
+            res = await self.es.search(index="integration_info", body=query)
+            if res['hits']['total']['value'] > 0:
+                return res['hits']['hits'][0]['_source'].get("status", "unknown")
+            else:
+                return "not_found"
+        except NotFoundError:
+            return "not_found"
+
     async def update_status(self, status: str):
-        await self.es.index(index="log_collection_status", id=self.system, body={"status": status}) # log_collection_status index 생성 수정 
-        
-    async def collect_logs(self):
-        pass
+        index_name = "integration_info"
+        await self.create_index_if_not_exists(index_name)
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"SYSTEM": self.system}},
+                        {"match": {"TAG_NAME": self.TAG_NAME}}
+                    ]
+                }
+            }
+        }
+        try:
+            res = await self.es.search(index=index_name, body=query)
+            if res['hits']['total']['value'] > 0:
+                doc_id = res['hits']['hits'][0]['_id']
+                await self.es.update(index=index_name, id=doc_id, body={"doc": {"status": status}})
+            else:
+                raise HTTPException(status_code=404, detail="TAG_NAME not found")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
-    def test(self):
-        pass
-        
+    async def update_integration(self, system, TAG_NAME, config):
+        index_name = "integration_info"
+        await self.create_index_if_not_exists(index_name)
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"SYSTEM": system}},
+                        {"match": {"TAG_NAME": TAG_NAME}}
+                    ]
+                }
+            }
+        }
+        try:
+            res = await self.es.search(index=index_name, body=query)
+            if res['hits']['total']['value'] > 0:
+                doc_id = res['hits']['hits'][0]['_id']
+                log = {
+                    "SYSTEM": system,
+                    "TAG_NAME": TAG_NAME,
+                    "inserted_at": datetime.datetime.now(),
+                    "status": "started",
+                    "config": config
+                }
+                response = await self.es.index(index=index_name, id=doc_id, document=log)
+                print(f"{index_name}_log: {response['result']}")
+                return response
+            else:
+                raise HTTPException(status_code=404, detail="TAG_NAME not found")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
-# @app.post("/add_integration/{system}")
-# async def add_integration(request, system):
-#     integration = ElasticsearchCollector(sytem=system)
-#     pass
+    async def delete_integration(self, system, TAG_NAME):
+        index_name = "integration_info"
+        await self.create_index_if_not_exists(index_name)
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"SYSTEM": system}},
+                        {"match": {"TAG_NAME": TAG_NAME}}
+                    ]
+                }
+            }
+        }
+        try:
+            res = await self.es.search(index=index_name, body=query)
+            if res['hits']['total']['value'] > 0:
+                doc_id = res['hits']['hits'][0]['_id']
+                response = await self.es.delete(index=index_name, id=doc_id)
+                print(f"{index_name}_log: {response['result']}")
+                return response
+            else:
+                raise HTTPException(status_code=404, detail="TAG_NAME not found")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
-# @app.post("/collector/{action}/{system}")
-# async def collector_action(request, system):
-#     action = ElasticsearchCollector(sytem=system)
-#     action.action()
-#     pass
-
-# @app.post("/collect_log/{system}")
-# async def add_integration(request, system):
-#     integration = ElasticsearchCollector(sytem=system)
-#     pass
+    async def manage_integration(self, action: str, config=None):
+        if action == "add":
+            await self.save_integration(self.system, self.TAG_NAME, config)
+        elif action == "start":
+            await self.update_status("started")
+        elif action == "stop":
+            await self.update_status("stopped")
+        elif action == "delete":
+            await self.delete_integration(self.system, self.TAG_NAME)
+        elif action == "update" and config:
+            await self.update_integration(self.system, self.TAG_NAME, config)
+        else:
+            raise HTTPException(status_code=400, detail="Invalid action or missing config for update")
