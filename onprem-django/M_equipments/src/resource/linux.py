67526a73c9bfca8a5f5944a900add4497b666c2c
chosomang <chosomang12@teiren.io>
2024-07-31 18:57:51 +0900

    update: updated integration

--- onprem-django/M_equipments/src/resource/linux.py
+++ onprem-django/M_equipments/src/resource/linux.py
@@ -1,45 +1,59 @@
 import os
 import requests
+import json
 import random
 import string
 import tempfile
 from django.http import FileResponse, HttpResponse
 from django.conf import settings
 
+class LinuxIntegration:
+    def __init__(self):
+        pass
+    
+    def check_agent(self):
+        pass
+    
+    def insert_agent(self):
+        try:
+            config = {
+                'server_ip':'',
+                'agent_port':'5140', 
+                'tag_name': f'linux_{''.join(random.choices(string.ascii_letters, k=5))}'.lower()
+            }
+            for item in ['server_ip', 'agent_port','tag_name']:
+                value = self.request.POST.get(item, '')
+                if value == '' and item == 'server_ip':
+                    return 'Please Insert Correct Server IP'
+                elif value != '':
+                    config[item] = value
+                    
+            file_path = os.path.join(settings.BASE_DIR, 'staticfiles', 'M_equipment', 'setup','setup_linux.sh')
+            with open(file_path, 'r') as file:
+                context = ''.join(file.readlines())
+                context = context.replace("{teiren_server_ip}", config['server_ip'])
+                context = context.replace("{agent_port}", config['agent_port'])
+                context = context.replace("{tag_name}", config['tag_name'])
+                context = context.encode('utf-8')
+            
+            filename = "teiren_linux_setup.sh"
+            temp = tempfile.NamedTemporaryFile(delete=False)        
+            temp.write(context)
+            temp.close()
+            return FileResponse(open(temp.name, 'rb'), as_attachment=True, filename=filename)
+        except Exception as e:
+            print(e)
+            
+    
 def linux_insert(request):
     try:
-        config = {
-            'server_ip':'',
-            'agent_port':'5140', 
-            'tag_name': f'linux_{''.join(random.choices(string.ascii_letters, k=5))}'.lower()
-        }
-        for item in ['server_ip', 'agent_port','tag_name']:
-            value = request.POST.get(item, '')
-            if value == '' and item == 'server_ip':
-                return 'Please Insert Correct Server IP'
-            elif value != '':
-                config[item] = value
-                
-        # FastAPI를 통해서 integration_info index에 잘 들어가는지 확인 
-        if linux_check() == "fail":
-            raise Exception
+        dict
         
-        file_path = os.path.join(settings.BASE_DIR, 'staticfiles', 'M_equipment', 'setup','setup_linux.sh')
-        with open(file_path, 'r') as file:
-            context = ''.join(file.readlines())
-            context = context.replace("{teiren_server_ip}", config['server_ip'])
-            context = context.replace("{agent_port}", config['agent_port'])
-            context = context.replace("{tag_name}", config['tag_name'])
-            context = context.encode('utf-8')
-        
-        filename = "teiren_linux_setup.sh"
-        temp = tempfile.NamedTemporaryFile(delete=False)        
-        temp.write(context)
-        temp.close()
-        return FileResponse(open(temp.name, 'rb'), as_attachment=True, filename=filename)
     except Exception as e:
         print(e)
-        return 'Wrong Configurations. Please Try Again'
+        response = {"error": 'Wrong Configurations. Please Try Again'}
+    finally:
+        return json.dumps(response)
 
 ##### 예외처리 / "/collector/{action}/{system}/{TAG_NAME}" 에 보내기
 def linux_check():
--- onprem-django/M_equipments/src/resource/mysql.py
+++ onprem-django/M_equipments/src/resource/mysql.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-class FortinetIntegration:
+class MySQLIntegartion:
     def __init__(self, request:dict):
         self.integration = request.pop('integration_type', '')
         self.request = request
@@ -18,26 +18,27 @@ class FortinetIntegration:
         return self.insert_network()
     
     # Elasticsearch Server에 연동하기 전에 예외처리 및 파싱 (API)
-    def check_api(self):
+    def check_account(self):
         """
         self.request = 
         {'access_key': '', 'dst_port': '', 'tag_name': ''}
         """
-        # for key, val in self.request.items():
-        #     if val == '':
-        #         return 'Please Insert All Required Items.'
-        # return 'api'
-        return {"error": "API Integration Not Ready. Please use Network Transmission."}
+        print(self.request)
+        return {"error": "Account Integration Not Ready. Please use Network Transmission."}
     
     # Elasticsearch Server에 연동하기 (TCP/UDP => fluentd)
     def insert_network(self):
+        """
+        self.request =
+        {'server_ip': '', 'dst_port': '', 'tag_name': ''}
+        """
         message_data = {
             "Teiren Server IP": self.request['server_ip'],
             "Destination Port": self.request['dst_port'],
             "Tag Name": self.request['tag_name']
         }
         try:
-            url = f"http://{self.request['server_ip']}:8088/collector/add/fortigate/{self.request['tag_name']}"
+            url = f"http://{self.request['server_ip']}:8088/collector/add/mysql/{self.request['tag_name']}"
             headers = {
                 "Content-Type": "application/json"
             }
@@ -49,21 +50,19 @@ class FortinetIntegration:
             }
             # response = requests.post(url=url, headers=headers, data=json.dumps(data))
             # print(response.json)
-            response = {"message": f"Successfully Integrated Fortinet NGFW: \n{message_data}"}
+            response = {"message": f"Successfully Integrated MySQL: \n{message_data}"}
         except Exception as e:
             print(e)
-            response = {"error": f"Failed to Integrate Fortinet NGFW: \n{message_data}"}
+            response = {"error": f"Failed to Integrate MySQL: \n{message_data}"}
         finally:
             return response
     
 
-def fortinet_insert(request):
-    # url = f"http://3.35.81.217:8088/fortigate_api_send?api_key={request.POST['access_key']}"
-    # response = requests.get(url)
+def mysql_insert(request):
     try:
         if request.POST.get('integration_type', '') == '':
             return 'Please reload the page and try again.'
-        integration =  FortinetIntegration(request=request.POST.dict())
+        integration =  MySQLIntegartion(request=request.POST.dict())
         response = getattr(integration, f'check_{integration.integration}')()
     except Exception as e:
         print(e)
