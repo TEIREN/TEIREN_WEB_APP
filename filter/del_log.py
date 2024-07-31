7db7062d9b227b71ddfddf98500e4a6b87959673
hw1186 <ro49165703@gmail.com>
2024-07-31 04:34:06 +0000

    git ignore

--- filter/del_log.py
+++ filter/del_log.py
@@ -14,6 +14,6 @@ delete_query = {
 }
 
 # delete_by_query API 호출
-response = es.delete_by_query(index='userinfo', body=delete_query)
+response = es.delete_by_query(index='integration_info', body=delete_query)
 
 print(response)
