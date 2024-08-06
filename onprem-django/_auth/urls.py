481044e216c3bfc4f6b90d7a28ea8b95b3550c76
chosomang <chosomang12@teiren.io>
2024-08-06 22:36:20 +0900

    .

--- onprem-django/_auth/urls.py
+++ onprem-django/_auth/urls.py
@@ -1,5 +1,4 @@
 from _auth import views
-from _auth.src.finevo import finevo
 from django.urls import path
 
 
@@ -8,6 +7,6 @@ urlpatterns = [
     # path('logout/', views.logout_, name='logout'),
     # path('register/', views.register_, name='register'),
     # path('activate/<str:uidb64>/<str:token>/', views.activate_, name='activate'),
-    path('login/', finevo.login_, name='login'),
-    path('logout/', finevo.logout_, name='logout'),
+    path('login/', views.login_, name='login'),
+    path('logout/', views.logout_, name='logout'),
 ]
