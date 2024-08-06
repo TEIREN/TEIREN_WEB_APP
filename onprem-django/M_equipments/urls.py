e867ea5f34263f3e0d2071523b8f024a764e3be2
chosomang <chosomang12@teiren.io>
2024-08-06 22:36:39 +0900

    update: Updating new integration page

--- onprem-django/M_equipments/urls.py
+++ onprem-django/M_equipments/urls.py
@@ -1,19 +1,10 @@
 # ~/integration/
 from django.urls import path
-from . import views
+from .views import IntegrationView
+from . import views_old
 
 urlpatterns = [
-    ## Configuration Ajax
-    # path('configuration/config/<actionType>/', views.integration_config_ajax),
-    
-    ## Registration Page/Ajax
-    path('<equipment>/<logType>/', views.registration_page),
-    path("<equipment>/<logType>/<actionType>/", views.integration_registration_ajax),
-    
-
-    
-    # Integration Page
-    # path('refresh/', views.refresh_integration_section, name='refresh-section'),
-    path('registration/', views.registration_view),
-    # path('configuration/', views.integration_view),
+    path('', IntegrationView.as_view(), name="integration"),
+    path('<system>/<log_type>/', IntegrationView.as_view(), name="register_page"),
+    path('<system>/<log_type>/<action_type>/', IntegrationView.as_view(), name="registration_ajax"),
 ]
